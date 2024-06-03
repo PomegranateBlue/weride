c4781e477d50c8c39c62a36fe92d8f2e2d8bc556
BLUE Pome <162660837+PomegranateBlue@users.noreply.github.com>
2024-06-03 18:08:02 +0900

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,13 +3,7 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 import firebase from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyAQwPECRPL8Hl5plN55XX8rfNUCBjj_tAY",
-  authDomain: "taxischedule-33477.firebaseapp.com",
-  projectId: "taxischedule-33477",
-  storageBucket: "taxischedule-33477.appspot.com",
-  messagingSenderId: "674791509208",
-  appId: "1:674791509208:web:266a4bdf796481c6035bad",
-  measurementId: "G-J11T1ZMY5D",
+//only my key
 };
 
 const app = initializeApp(firebaseConfig);
