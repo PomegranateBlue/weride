91731bfbd53f496e6f8b349fa4c7f7815ee8f326
bluepome98 <wpdn1998@gmail.com>
2024-10-09 23:00:38 +0900

    로그인 화면 초석

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,25 @@
 import React, { useState } from "react";
 import { auth, realTimeDB } from "../firebase.js";
-import { signInWithEmailAndPassword,getAuth } from "firebase/auth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 import { ref, set } from "firebase/database";
-import { v4 as uuidv4 } from "uuid";
+//import { v4 as uuidv4 } from "uuid";
 //import Button from "@mui/material/Button";
-import "../styles/profile.css";
+import "../styles/Login.css";
+import { useForm } from "react-hook-form";
 
 const LoginComponent = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phonenumber, setPhonenumber] = useState("");
-
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
-    <form>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="이메일"
-      ></input>
-      <input
-        type="password"
-        value="{password}"
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="비밀번호"
-      ></input>
-      <input
-        type="text"
-        value={phonenumber}
-        onChange={(e) => setPhonenumber(e.target.value)}
-        placeholder="전화번호"
-      ></input>
-      <button type="submit">서비스 가입</button>
-    </form>
+    <div>
+      <h1 id="title">WERIDE</h1>
+      <p id="welcome_line">
+        WERIDE를 이용해주셔서 감사합니다<br></br>
+        택시 동승 서비스를 이용해보세요<br></br>
+        함께 편안한 출퇴근길이 되길 바랍니다
+      </p>
+      <div>이곳에 로그인 양식과 회원가입 리디렉션 버튼</div>
+    </div>
   );
 };
 
