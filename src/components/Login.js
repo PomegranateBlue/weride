821cb86b3b88c526aef8ac84c6d578690fd737ef
bluepome98 <wpdn1998@gmail.com>
2024-10-15 16:04:29 +0900

    로그인 컴포넌트 화면 개발

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,8 +8,15 @@ import "../styles/Login.css";
 import { useForm } from "react-hook-form";
 
 const LoginComponent = () => {
-  const { register, handleSubmit } = useForm();
+  //const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
     <div>
       <h1 id="title">WERIDE</h1>
@@ -18,7 +25,22 @@ const LoginComponent = () => {
         택시 동승 서비스를 이용해보세요<br></br>
         함께 편안한 출퇴근길이 되길 바랍니다
       </p>
-      <div>이곳에 로그인 양식과 회원가입 리디렉션 버튼</div>
+      <div className="login-container">
+        <div className="login-form">
+          <label>이메일</label>
+          <input className="email-form" placeholder="email@email.com"></input>
+          <label>비밀번호</label>
+          <input
+            className="password-form"
+            placeholder="비밀번호를 입력하세요"
+          ></input>
+          <button id="loginBtn">로그인</button>
+          <div className="signInInfo">
+            <label>회원등록 후 이용가능합니다</label>
+            <button id="signInBtn">회원가입</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
