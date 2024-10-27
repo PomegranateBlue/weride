5a9727ccc5ca7b0b6235208eb4fff2d127397b4a
bluepome98 <wpdn1998@gmail.com>
2024-10-27 23:03:43 +0900

    로그인 컴포넌트 CSS수정전

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,55 @@
 import React, { useState } from "react";
 //import { auth, realTimeDB } from "../firebase.js";
-//import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { auth } from "../firebase";
 //import { ref, set } from "firebase/database";
 //import { v4 as uuidv4 } from "uuid";
 //import Button from "@mui/material/Button";
 import "../styles/Login.css";
-import { useForm } from "react-hook-form";
+//import { useForm } from "react-hook-form";
 import SignUpModal from "./SignUpModal";
 
 const LoginComponent = () => {
   //const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  //const onSubmit = (data) => console.log(data);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [errorMessage, setErrorMessage] = useState("");
   const [isModalOpen, setModalOpen] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    if (!email || !password) {
+      setErrorMessage("이메일과 비밀번호를 입력하세요");
+      return;
+    }
+    //console.log(email, password);
+    setErrorMessage("");
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("로그인 성공");
+    } catch (error) {
+      if (error.code === "auth/wrong-email") {
+        setErrorMessage("형식에 맞는 이메일을 입력해주세요");
+      } else if (error.code === "auth/user-not-found") {
+        setErrorMessage("해당하는 이용자가 없습니다. 회원가입 후 시도해주세요");
+      } else if (error.code === "auth/wrong-password") {
+        setErrorMessage("비밀번호가 틀렸습니다. 다시 시도하세요");
+      } else {
+        setErrorMessage("로그인 문제 발생, 문의바랍니다.");
+      }
+    }
   };
 
-  const handleSignUpClick = () => {
-    console.log("회원가입 버튼");
+  const modalOpen = () => {
     setModalOpen(true);
   };
 
-  const closeModal = () => {
+  const modalClose = () => {
     setModalOpen(false);
   };
+
   return (
     <div>
       <h1 id="title">WERIDE</h1>
@@ -39,20 +60,46 @@ const LoginComponent = () => {
       </p>
       <div className="login-container">
         <div className="login-form">
-          <label>이메일</label>
-          <input className="email-form" placeholder="email@email.com"></input>
-          <label>비밀번호</label>
-          <input
-            className="password-form"
-            placeholder="비밀번호를 입력하세요"
-          ></input>
-          <button id="loginBtn">로그인</button>
-          <div className="signInInfo">
-            <div className="signInfo">회원등록 후 이용가능합니다</div>
-            <button id="signInBtn" onClick={handleSignUpClick}>
-              회원가입
+          <form>
+            <div className="userInput">
+              {" "}
+              <label>이메일</label>
+              <input
+                className="email-form"
+                placeholder="email@email.com"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              ></input>
+              <label>비밀번호</label>
+              <input
+                className="password-form"
+                placeholder="비밀번호를 입력하세요"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              ></input>
+            </div>
+
+            {errorMessage && (
+              <p style={{ color: "red", fontWeight: "800" }}>{errorMessage}</p>
+            )}
+            <button id="loginBtn" onClick={handleSubmit}>
+              로그인
             </button>
-          </div>
+            <div className="signInInfo">
+              <div className="signInfo">회원등록 후 이용가능합니다</div>
+              <button id="signInBtn" onClick={modalOpen}>
+                회원가입
+              </button>
+              {isModalOpen && (
+                <SignUpModal
+                  isModalOpen={isModalOpen}
+                  modalClose={modalClose}
+                />
+              )}
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/components/SignUpModal.js
+++ src/components/SignUpModal.js
@@ -1,40 +1,63 @@
 import React, { useState } from "react";
-import "../styles/modal.css";
-const SignUpModal = ({ closeModal }) => {
-  const [email, setEmail] = useState("");
-  const [passwd, setPasswd] = useState("");
-  const [error, setError] = useState("");
+import "../styles/modal.css"; // 모달 스타일 파일 임포트
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
-  const handleSignup = (e) => {
+const SignUpModal = ({ isModalOpen, modalClose }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const modalSignUp = async (e) => {
     e.preventDefault();
-    closeModal();
+    setErrorMessage("");
+    setSuccessMessage("");
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      setSuccessMessage("회원이 되신 걸 축하드립니다.");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
   };
 
   return (
-    <div className="modal-container">
+    <div
+      className="modal-container"
+      style={{ display: isModalOpen ? "flex" : "none" }}
+    >
       <div className="modal-content">
-        <h2>회원가입</h2>
-        {error && <p className="error-mesg">{error}</p>}
-        <form onSubmit={handleSignup}>
+        <form className="modalSignupform" onSubmit={modalSignUp}>
           <label>이메일</label>
           <input
+            className="modal-email-form"
+            placeholder="email@email.com"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="email@email.com"
             required
           ></input>
           <label>비밀번호</label>
           <input
-            type="password"
-            value={passwd}
-            onChange={(e) => setPasswd(e.target.value)}
+            className="modal-password-form"
             placeholder="비밀번호를 입력하세요"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
           ></input>
-          <button type="submit">회원가입</button>
+          {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+          {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+          <button className="modal-submitBtn" type="submit">
+            제출하기
+          </button>
         </form>
-        <button onClick={closeModal}>닫기</button>
+        <button className="closeBtn" onClick={modalClose}>
+          닫기
+        </button>
       </div>
     </div>
   );
