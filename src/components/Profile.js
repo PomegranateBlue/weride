3fa5289cde9aef999a9b0ff93bea3da3a15374b7
bluepome98 <wpdn1998@gmail.com>
2024-10-30 19:49:10 +0900

    사용자 전용 페이지

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
-
+import "../styles/profile.css";
 const ProfileComponent = () => {
   return (
     <div>
-      <p>사용자 정보 수정페이지</p>
+      <p className="profile-title">사용자 전용 페이지</p>
+      <p className="profile-info">이용할 서비스를 선택하세요</p>
+      <div className="profile-menu-container">
+        <button>실시간 예약</button>
+        <button>일자별 예약</button>
+        <button>사용자 정보 수정</button>
+      </div>
     </div>
   );
 };
