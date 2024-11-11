9f1b5e5b98efe0599d50851eb2412bd927e7de71
bluepome98 <wpdn1998@gmail.com>
2024-11-11 17:56:57 +0900

    일자별 예약 기능 개발

--- src/components/DayTime.js
+++ src/components/DayTime.js
@@ -1,34 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, forwardRef } from "react";
 import DatePicker from "react-datepicker";
+import { ko } from "date-fns/locale";
 import "react-datepicker/dist/react-datepicker.css";
 import ReserveModal from "./ReserveModal";
+import "../styles/dayTime.css";
 const DayTimeComponent = () => {
-  const [selectDay, setSelectDay] = useState(null);
+  const [modalSelectDay, setModalSelectDay] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const [startDate, setStartDate] = useState(new Date());
+  const CustomDatepicker = forwardRef(({ value, onClick, className }, ref) => (
+    <button className="custom-datePicker" onClick={onClick} ref={ref}>
+      {value}
+    </button>
+  ));
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
   return (
     <div className="daytime-container">
-      <h2>원하는 예약 날짜와 조건을 설정하세요.</h2>
-      <DatePicker
-        selected={selectDay}
-        onChange={(date) => {
-          setSelectDay(date);
-          openModal();
-        }}
-        dateFormat="yyyy-MM-dd"
-        minDate={new Date()}
-        className="datePicker-custom"
-        calendarClassName="custom-calendar"
-      ></DatePicker>
-      {isModalOpen && (
-        <ReserveModal
-          mode="date"
-          selectDay={selectDay}
-          closeModal={closeModal}
-        ></ReserveModal>
-      )}
+      <div className="daytime-content">
+        <h1 className="componentTitle">일자별 예약</h1>
+        <h2 className="componentInfo">원하는 예약 날짜와 조건을 설정하세요.</h2>
+        <div className="datePicker-container">
+          <DatePicker
+            className="datePicker"
+            selected={startDate}
+            onChange={(date) => {
+              setStartDate(date);
+              setModalSelectDay(date);
+              openModal();
+            }}
+            dateFormat="yyyy-MM-dd"
+            locale={ko}
+            customInput={<CustomDatepicker />}
+          ></DatePicker>
+        </div>
+        <div className="user-reservation"></div>
+        {isModalOpen && (
+          <ReserveModal
+            mode="date"
+            selectDay={modalSelectDay}
+            closeModal={closeModal}
+          ></ReserveModal>
+        )}
+      </div>
     </div>
   );
 };
